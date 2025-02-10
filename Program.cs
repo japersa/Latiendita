5c1c7dde029903be0ba53a05c1291caf5dee19ee
ludekabeko <luedbeltrancolon@gmail.com>
2025-02-10 18:57:24 -0500

    Program.cs modificado y organizar swagger

--- Program.cs
+++ Program.cs
@@ -2,64 +2,51 @@ using Latiendita.Repositories;
 using Latiendita.Data;
 using Microsoft.EntityFrameworkCore;
 using Latiendita.Services;
-using NLog.Web;
-using NLog;
-
-var logger = NLog.LogManager.Setup()
-    .LoadConfigurationFromAppSettings().GetCurrentClassLogger();
-logger.Debug("Init main");
-
-try 
+ 
+var builder = WebApplication.CreateBuilder(args);
+ 
+ 
+// Conexion a la BD
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+ 
+ 
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+ 
+ 
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+ 
+ 
+// Add services to the container.
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+builder.Services.AddScoped<ISaleService, SaleService>();
+ 
+ 
+var app = builder.Build();
+ 
+ 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    var builder = WebApplication.CreateBuilder(args);
-
-    // Add services to the container.
-    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-    builder.Services.AddOpenApi();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-    builder.Services.AddScoped<ICategoryService, CategoryService>();
-    builder.Services.AddScoped<IProductService, ProductService>();
-    builder.Services.AddScoped<IProductRepository, ProductRepository>();
-    builder.Services.AddScoped<IUserRepository, UserRepository>();
-    builder.Services.AddScoped<IUserService, UserService>();
-    builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-    builder.Services.AddScoped<IOrderService, OrderService>();
-    builder.Services.AddScoped<ISaleRepository, SaleRepository>();
-    builder.Services.AddScoped<ISaleService, SaleService>();
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
-
-    builder.Logging.ClearProviders();
-    builder.Host.UseNLog();
-
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    var app = builder.Build();
-    app.MapControllers();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.MapOpenApi();
-        app.UseSwagger();
-        app.UseSwaggerUI();
-
-    }
-
-    app.UseHttpsRedirection();
-
-    app.Run();
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-catch (Exception ex)
-{
-    logger.Error(ex, "Stopped program because of exception");
-    throw;
-}
-finally
-{
-    NLog.LogManager.Shutdown();
-}
-
+ 
+app.UseHttpsRedirection();
+app.MapControllers();
+ 
+ 
+app.Run();
