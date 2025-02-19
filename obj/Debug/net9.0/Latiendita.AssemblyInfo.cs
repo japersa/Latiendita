98a12eaeb9d72d3c756ecb8be5577783c7d3d473
Alejogm2022 <alejandro.gomezmeneses@gmail.com>
2025-02-18 21:04:51 -0500

    update project

--- obj/Debug/net9.0/Latiendita.AssemblyInfo.cs
+++ obj/Debug/net9.0/Latiendita.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Latiendita")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc22a4c64b2f3c34aecd8ce8c0d0aee1b0a44453")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ac5b7b027b37752edf714f6ab948a39533d4beb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Latiendita")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Latiendita")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
